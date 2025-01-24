8aa94cf4f89efa9049e3e25b1bf3dbf15a22dc9f
adam west <adammwest1@gmail.com>
2025-01-24 03:25:02 +0100

    update graphs

--- run_analysis.py
+++ run_analysis.py
@@ -19,7 +19,15 @@ args = parse_arguments()
 root_path = __file__.replace("run_analysis.py",f"/data/bitaxe_benchmark_results_{args.bitaxe_ip}.json")
 with open(root_path) as data:
     jdata = json.load(data)
-    df = pd.json_normalize(jdata['all_results'])
+    try:
+        df = pd.json_normalize(jdata['all_results'])
+    except:
+        try:
+            df = pd.json_normalize(jdata)
+        except:
+            print("cant read file")
+            raise ValueError("json not in expected form")
+
 
 
 OPT = parametrized_model([[1000,1350],[450,610]],1276)
@@ -42,34 +50,43 @@ best_hist = OPT.history
 for i in OPT.history:
     for j in OPT.history:
         if i != j:
-    
             history = [i,j]
-            OPT.reparametrize_T_eqn(history)
-            OPT.reparametrize_vmin_eqn(history)
-            OPT.reparametrize_t0_eqn(history)
-            OPT.reparametrize_vpower(history)
-            hash_rate_err= 0
-            for core,freq,temp,hashrate in data:
-                ypred = OPT.model([core,freq])
-                errH = abs(hashrate-ypred[0])
-                errT = abs(temp-ypred[1])
-                hash_rate_err+=errH
+            try:
+                OPT.reparametrize_T_eqn(history)
+                OPT.reparametrize_vmin_eqn(history)
+                OPT.reparametrize_t0_eqn(history)
+                OPT.reparametrize_vpower(history)
+                hash_rate_err= 0
+                for core,freq,temp,hashrate in data:
+                    ypred = OPT.model([core,freq])
+                    errH = abs(hashrate-ypred[0])
+                    errT = abs(temp-ypred[1])
+                    hash_rate_err+=errH
 
-            if hash_rate_err<min_err:
-                min_err = hash_rate_err
-                best_hist = [i,j]
+                if hash_rate_err<min_err:
+                    min_err = hash_rate_err
+                    best_hist = [i,j]
+            except ZeroDivisionError:
+                # this occours in reparametrize T
+                pass
 
 OPT.reparametrize_T_eqn(best_hist)
 OPT.reparametrize_vmin_eqn(best_hist)
 OPT.reparametrize_t0_eqn(best_hist)
 OPT.reparametrize_vpower(best_hist)
+#OPT.k_fvoffset=0.0
+#OPT.k_fvmult = 0.0
+#OPT.k_tmult = 0.0
+#OPT.k_toffset =0.0
+#OPT.tpower = 0.0
+#OPT.vpower = 0.0
 
 
 print("\nLearned voltage and temperature penalty parameters")
-print('Voltage penalty p * (F * k + c )')
+print('Voltage penalty p * ((F * k + c) - V)')
 print(f"p={OPT.vpower:.5} k={OPT.k_fvmult:.5} c={OPT.k_fvoffset:.5}")
 print('\nTemp penalty  p * (T0 - (V * k + c))')
-print(f"p={OPT.tpower:.5} k={OPT.k_tmult:.5} c={OPT.k_fvoffset:.5}")
+print(f"p={OPT.tpower:.5} k={OPT.k_tmult:.5} c={OPT.k_toffset:.5}")
 print(f'\nT0={float(OPT.t0):.5}')
 
 hash_rate_err= 0
@@ -84,13 +101,13 @@ print(f'Hashrate err ={errH/len(data)}')
 print(f'Temp err     ={errT/len(data)}')
 
 # Calulate best postition from model
-best_p = OPT.maximise_eqn()
+best_loc = OPT.maximise_eqn()
 
 print("\nThe Best Positions")
 print("Vcore Freq Hashrate")
 print("    Type   V    F   H")
 print("Benchmark ",int(OPT.get_best()[0][0]),int(OPT.get_best()[0][1]),int(OPT.get_best()[1]))
-print("    Model ",best_p[0],best_p[1],int(OPT.model(best_p)[0]))
+print("    Model ",best_loc[0],best_loc[1],int(OPT.model(best_loc)[0]))
 
 to_graph = False
 try:
@@ -114,19 +131,15 @@ if to_graph:
     grid = [(v, f) for v in vcore_values for f in f_values]
     predicted_scores = [OPT.model(x)[0] for x in grid]
     vcore_grid, f_grid = np.meshgrid(vcore_values, f_values)
-    predicted_grid = np.array(predicted_scores).reshape(len(f_values), len(vcore_values))
+    predicted_grid = np.array(predicted_scores).reshape(len(f_values),len(vcore_values)).T
 
     # Normalize to the combined range of predicted and real data
     min_val = min(predicted_grid.min(), df['averageHashRate'].min())
     max_val = max(predicted_grid.max(), df['averageHashRate'].max())
     norm = Normalize(vmin=min_val, vmax=max_val)
 
-
-
-    # Make graph
-    plt.figure(figsize=(8, 6))
-
     # Plot the predicted scores as a contour plot
+    plt.figure(figsize=(8, 6))
     contour = plt.contourf(vcore_grid, f_grid, predicted_grid, cmap='viridis', norm=norm, alpha=0.7)
 
     # Overlay the real benchmarked data as a scatter plot
@@ -139,18 +152,19 @@ if to_graph:
         edgecolor='k',
     )
 
+    plt.plot(best_loc[0], best_loc[1], marker='*', color='red', markersize=10, label='Best Position')
+
+
     # Add a single shared color bar for both plots
     cbar = plt.colorbar(contour)
     cbar.set_label('Hashrate')
-
-    # Titles and labels
     plt.title('Hashrate Model (Translucent) and Benchmarked Data')
     plt.xlabel('Vcore (mV)')
     plt.ylabel('Frequency (MHz)')
-
     plt.tight_layout()
 
     # Save the plot to a file
-    output_file = "plot.png"  # Replace with your desired filename
+    output_file = f"plot_{args.bitaxe_ip}.png"  # Replace with your desired filename
     plt.savefig(output_file)
+    print("\nGraph")
     print(f"Plot saved to {output_file}")
